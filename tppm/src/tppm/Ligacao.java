19cad534c385cbdf8834fd8d2decb3dc6389b358
Priscilla <Priscilla@Priscilla-PC>
2015-06-02 17:42:33 -0300

    testeeee

--- tppm/src/tppm/Ligacao.java
+++ tppm/src/tppm/Ligacao.java
@@ -6,21 +6,16 @@
 package tppm;
 
 public class Ligacao {
-	private String central_A;
-	private String central_B;
-	//O construtor cria uma linha desocupada entre as centrais A e B.
-	public Ligacao(String centralA, String centralB) {
-		this.central_A = centralA;
-		this.central_B = centralB;
+	
+	private String assinateOrigem;
+	private String assinanteDest;
+	private int duracao;
+	private int valorChamada;
+	
+	//constructor
+	public Ligacao(){
+		
 	}
-	//Retorna central conectada com central de label
-	public Central getCentralConectada (String label){
-		return null;
-	}
-	public String getCentral_A() {
-		return central_A;
-	}
-	public void setCentral_A(String central_A) {
-		this.central_A = central_A;
-	}
+	
+
 }
--- tppm/src/tppm/Rede.java
+++ tppm/src/tppm/Rede.java
@@ -1,9 +1,10 @@
 /* 
- * Essa classe é responsável por criar todas as conexões a partir de um arquivo de entrada. 
- * Ela é a classe de maior nível e controla toda a rede telefônica. 
- * Após a leitura do arquivo, ela cria as centrais e seus respectivos clientes, as linhas que conectam duas centrais e 
-      recebe todas as tentativas de chamada. Tem o papel de relacionar os módulos, consultar a central, 
-      retornar a central desejada com os clientes, fazer a conexão entre dois clientes e remover esta conexão. 
+ * 
+Essa classe ï¿½ responsï¿½vel por criar todas as conexï¿½es a partir de um arquivo de entrada. 
+ * Ela ï¿½ a classe de maior nï¿½vel e controla toda a rede telefï¿½nica. 
+ * Apï¿½s a leitura do arquivo, ela cria as centrais e seus respectivos clientes, as linhas que conectam duas centrais e 
+      recebe todas as tentativas de chamada. Tem o papel de relacionar os mï¿½dulos, consultar a central, 
+      retornar a central desejada com os clientes, fazer a conexï¿½o entre dois clientes e remover esta conexï¿½o. 
 */
 
 package tppm;
@@ -11,25 +12,26 @@ package tppm;
 import java.util.ArrayList;
 
 public class Rede {
-	//Pesquisa em todas as centrais. Se encontrar, faz a atribuição do label e retorna true. Caso contrário retorna false.
-	public boolean consultaCentral (String label) {		
+	
+	public boolean consultaCentral (String id) {
+		
 		return false;
 	}
 	
-	//Pesquisa em todas as centrais. Se encontrar, faz a atribuição do label e retorna a central. Caso contrário retorna null.
-	public String getCentral (String label) {  //Dúvida no tipo de retorno (deve retornar uma central)
+	public String getCentral (String id) {  //Dï¿½vida no tipo de retorno (deve retornar uma central)
 		
 		return null;
 	}
 	
-	//Pesquisa. Se a centralA ou a centralB não existir, não é estabelecido a conexão e retorna null. Se existir, deve-se olhar o status do AssinanteA e do AssinanteB (se existem e estão disponíveis) e se há um caminho entre eles. Se sim, o cliente recebe o tom de discagem/tocando e a função retorna o caminho. Se não, envia o sinal de ocupado e retorna null.
-	public String getConexao (String assinanteA, String centralA, String assinanteB, String centralB){  //Dúvida no tipo de retorno (deve retornar o caminho ou null)
+	public String getConexao (String assinanteA, String centralA, String assinanteB, String centralB){  //Dï¿½vida no tipo de retorno (deve retornar o caminho ou null)
 		
 		return " ";
 	}
 	
-	//Pesquisa se a central e o caminho existem. Se não, indica que não existe conexão e retorna falso. Se existir e o cliente que ligou desligar ou o cliente que recebeu a ligação desligá-a e passar os 90 segundos, a conexão é encerrada, retornando true. Se o cliente que recebeu a ligação desligar e pegar o telefone novamente dentro dos 90 segundos, a conexão continua, retornando false. 
-	public boolean fechaConexao (String assinanteA, String AssinanteB, String central, ArrayList<Integer> caminho, int tempo) {	
+	public boolean fechaConexao (String assinanteA, String AssinanteB, String central, ArrayList<Integer> caminho, int tempo) {
+		
 		return false;		
 	}
+	
+	
 }
