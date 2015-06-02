dad4be89cd88e885845fd9e579c2287ac4fb0755
Priscilla <Priscilla@Priscilla-PC>
2015-06-02 13:07:45 -0300

    Mudança da estrutura

--- tppm/src/tppm/Assinante.java
+++ tppm/src/tppm/Assinante.java
@@ -1,10 +1,20 @@
+/* 
+ * Essa classe é responsável por armazenar os dados do cliente, a quantidade de ligações realizadas, 
+     se este está em uma ligação, se seu telefone está tocando e se está ocupado. 
+ * Tendo funções para retornar cada um destes elementos, funções para modificar as variáveis que podem ser alteradas 
+     e uma função que retorna se o cliente pode receber ou não uma requisição de chamada (getAvailable).
+*/
+
 package tppm;
 
-public class Assinante {
+public class Assinante { 
 	private String nome;
 	private int id;
 	private String tel;
 	
+	public Assinante() {
+		// TODO Auto-generated constructor stub
+	}
 	public String getNome() {
 		return nome;
 	}
@@ -23,5 +33,4 @@ public class Assinante {
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
-	
 }
---
+++ tppm/src/tppm/Rede.java
@@ -0,0 +1,25 @@
+/* 
+ * Essa classe é responsável por criar todas as conexões a partir de um arquivo de entrada. 
+ * Ela é a classe de maior nível e controla toda a rede telefônica. 
+ * Após a leitura do arquivo, ela cria as centrais e seus respectivos clientes, as linhas que conectam duas centrais e 
+      recebe todas as tentativas de chamada. Tem o papel de relacionar os módulos, consultar a central, 
+      retornar a central desejada com os clientes, fazer a conexão entre dois clientes e remover esta conexão. 
+*/
+
+package tppm;
+
+public class Rede {
+	public boolean consultaCentral (String id) {
+		
+		return false;
+	}
+	public String getCentral (String id) {  //Dúvida no tipo de retorno (deve retornar uma central)
+		
+		return null;
+	}
+	public String getConexao (String assinanteA, String centralA, String assinanteB, String centralB){  //Dúvida no tipo de retorno (deve retornar o caminho ou null)
+		return " ";
+		
+	}
+	
+}
