424c7f9e33acd8915bdde6cc13109de6a0447d76
Priscilla <Priscilla@Priscilla-PC>
2015-06-02 16:04:54 -0300

    Classe Assinante estruturada.

--- tppm/src/tppm/Assinante.java
+++ tppm/src/tppm/Assinante.java
@@ -9,8 +9,12 @@ package tppm;
 
 public class Assinante { 
 	private String nome;
-	private int id;
+	//private int id;
 	private String tel;
+	private boolean ocupado;
+	private boolean emLigacao;
+	private boolean telTocando;
+	private int impulsos;
 	
 	public Assinante() {
 		// TODO Auto-generated constructor stub
@@ -21,16 +25,51 @@ public class Assinante {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+//	public int getId() {
+//		return id;
+//	}
+//	public void setId(int id) {
+//		this.id = id;
+//	}
 	public String getTel() {
 		return tel;
 	}
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
+	public boolean getDisponivel () {
+		if (!isEmLigacao() && !isTelTocando())
+			return true;
+		else
+			return false;
+	}
+	public boolean isOcupado() {
+		return ocupado;
+	}
+	public void setOcupado(boolean ocupado) {
+		this.ocupado = ocupado;
+	}
+	public boolean isEmLigacao() {
+		return emLigacao;
+	}
+	public void setEmLigacao(boolean emLigacao) {
+		this.emLigacao = emLigacao;
+	}
+	public boolean isTelTocando() {
+		return telTocando;
+	}
+	public void setTelTocando(boolean telTocando) {
+		this.telTocando = telTocando;
+	}
+	public int getImpulsos() {
+		return impulsos;
+	}
+	public void setImpulsos(int impulsos) {
+		this.impulsos = impulsos;
+	}
+	public void imprimeMsg(String msg){
+		System.out.println("Assinante: " + getNome() + " Mensagem: " + msg);
+	}
+	
+	
 }
