3aa2573e31d8cd5701e632f473b61d35884adfca
Priscilla Vasconcelos <pfvasconcelos@yahoo.com.br>
2015-06-03 10:13:34 -0300

    Create Central.java
    add a classe Central.java  (Não entendi, mas ela sumiu).

---
+++ tppm/src/tppm/Central.java
@@ -0,0 +1,11 @@
+/*
+ * Essa classe e responsavel por armazenar o nome da central, todos os seus clientes e todas as linhas que esta central possui. 
+ * Tambem e responsavel pela manutencao dos clientes e linhas entre as centrais, enviar contas para os clientes e 
+  	  conectar seus clientes com os clientes de outras centrais.
+*/
+
+package tppm;
+
+public class Central {
+
+}
